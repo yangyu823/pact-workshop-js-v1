898663db2f8c82210de943dfb7ad14bfaced7ca0
Yu Yang <yangyu823@gmail.com>
2020-05-26 21:32:16 +1000

    set consumer version

--- consumer/test/publish.js
+++ consumer/test/publish.js
@@ -5,7 +5,7 @@ const gitHash = require('child_process')
     .toString().trim();
 const opts = {
   pactFilesOrDirs: [path.resolve(__dirname, '../../pacts/')],
-  pactBroker: 'http://35.244.122.147/',
+  pactBroker: 'http://35.244.125.151/',
   pactBrokerUsername: 'CSPUsr2',
   pactBrokerPassword: 'Password1',
   tags: ['prod', 'test'],
@@ -17,7 +17,7 @@ pact
   .then(() => {
     console.log('Pact contract publishing complete!')
     console.log('')
-    console.log('Head over to http://35.244.122.147/ and login with')
+    console.log('Head over to http://35.244.125.151/ and login with')
     console.log('=> Username: CSPUsr2')
     console.log('=> Password: Password1')
     console.log('to see your published contracts.')
